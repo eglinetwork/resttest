ba40c2572831d7433f8738d5cbaa3cbb8656a022
nyfelix <nyfelix@gmail.com>
2011-06-17 08:47:46 -0700

    Basic Layout of the application

--- static/js/index.js
+++ static/js/index.js
@@ -3,8 +3,51 @@
  * Besides building a great application I use this opportunity to get familiar with YUI3
  */
 
-YUI().use('node', 'tabview', 'io', function(Y) {
-    var tabviewRequest = new Y.TabView({
+YUI({
+    modules: {
+        'rt_service': {
+            fullpath: '/js/rt_service.js',
+            requires : ['base', 'widget']
+        },
+        'rt_datamanager': {
+            fullpath: '/js/rt_datamanager.js',
+            requires : ['base', 'io']
+        }
+    }
+}).use('base','node','rt_service', 'rt_datamanager', function(Y) {
+    function main() {
+        Y.one('.yui3-js-enabled').removeClass('yui3-js-enabled');
+        Y.log('it is ready');
+
+        // Load the services and create instances of rt_service
+        var dataManager = new Y.RT.DataManager();       
+        var data =  dataManager.loadProject('0001');
+        
+
+            Y.Array.each (data.services, function(service) {
+                var myService = new Y.RT.Service();
+                myService.render('#services');
+                myService.set('name', service.name);
+                myService.set('description', service.description);
+            })
+
+    }
+    Y.on("domready", main);
+});
+
+
+
+
+
+
+
+
+/*YUI().use('node', 'tabview', 'io', 'gallery-storage-lite', function(Y) {
+    Y.namespace('RT');
+
+    Y.RT.data = {}
+    
+    Y.RT.tabviewRequest = new Y.TabView({
         srcNode: '#requesttab',
         children: [{
             label: 'Content',
@@ -18,20 +61,18 @@ YUI().use('node', 'tabview', 'io', function(Y) {
         }]
     });
 
-    var tabviewResponse = new Y.TabView({
+    Y.RT.tabviewResponse = new Y.TabView({
         srcNode: '#responsetab',
         children: [{
-            label: 'Content',
+            label: 'Plaintext',
             content: '<textarea class="responsecontent"></textarea>'
         }, {
-            label: 'Headers',
-            content: '<textarea class="responsecontent"></textarea>'
-        }, {
-            label: 'Paramter',
+            label: 'Testcases',
             content: '<textarea class="responsecontent"></textarea>'
         }]
     });
-    
-    tabviewRequest.render();
-    tabviewResponse.render();
-});
+
+    Y.RT.tabviewRequest.render();
+    Y.RT.tabviewResponse.render();
+});*/
+
